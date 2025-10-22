b1b6fe848ec12e1805db77804038fe3acbcf9df8
IstosMedical <mazhar@istos.in>
2025-10-22 15:06:15 +0530

    Update products.js

--- products.js
+++ products.js
@@ -3,18 +3,18 @@ const products = [
     id: 1,
     name: "Toothpaste Value Pack (3x150g)",
     price: 199,
-    image: "images/toothpaste.png"
+    image: "images/toothpaste.jpg"
   },
   {
     id: 2,
     name: "Dish Soap Mega Pack (2L)",
     price: 149,
-    image: "images/dishsoap.png"
+    image: "images/dishsoap.jpg"
   },
   {
     id: 3,
     name: "Shampoo Combo (2x500ml)",
     price: 299,
-    image: "images/shampoo.png"
+    image: "images/shampoo.jpg"
   }
 ];
