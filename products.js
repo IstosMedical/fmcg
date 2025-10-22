fecc4c08536a8499f3617076ff7916d8e8efe4ec
IstosMedical <mazhar@istos.in>
2025-10-22 17:45:24 +0530

    Update products.js

--- products.js
+++ products.js
@@ -4,20 +4,71 @@ const products = [
     name: "Toothpaste Value Pack (3x150g)",
     price: 199,
     image: "images/toothpaste.jpg",
-    tag: "🔥 Fast Moving"
+    tag: "🔥 Fast Moving",
+    category: "Hygiene"
   },
   {
     id: 2,
     name: "Dish Soap Mega Pack (2L)",
     price: 149,
     image: "images/dishsoap.jpg",
-    tag: "⏳ Limited Stock"
+    tag: "⏳ Limited Stock",
+    category: "Hygiene"
   },
   {
     id: 3,
     name: "Shampoo Combo (2x500ml)",
     price: 299,
     image: "images/shampoo.jpg",
-    tag: "💥 Deal of the Day"
+    tag: "💥 Deal of the Day".
+    category: "Hygiene"
+  }
+    {
+    id: 1,
+    name: "Toothpaste Value Pack (3x150g)",
+    price: 199,
+    image: "images/toothpaste.jpg",
+    tag: "🔥 Fast Moving",
+    category: "Hygiene"
+  },
+  {
+    id: 2,
+    name: "Dish Soap Mega Pack (2L)",
+    price: 149,
+    image: "images/dishsoap.jpg",
+    tag: "⏳ Limited Stock",
+    category: "Hygiene"
+  },
+  {
+    id: 3,
+    name: "Shampoo Combo (2x500ml)",
+    price: 299,
+    image: "images/shampoo.jpg",
+    tag: "💥 Deal of the Day".
+    category: "Hygiene"
+  }
+  {
+    id: 1,
+    name: "Toothpaste Value Pack (3x150g)",
+    price: 199,
+    image: "images/toothpaste.jpg",
+    tag: "🔥 Fast Moving",
+    category: "Hygiene"
+  },
+  {
+    id: 2,
+    name: "Dish Soap Mega Pack (2L)",
+    price: 149,
+    image: "images/dishsoap.jpg",
+    tag: "⏳ Limited Stock",
+    category: "Hygiene"
+  },
+  {
+    id: 3,
+    name: "Shampoo Combo (2x500ml)",
+    price: 299,
+    image: "images/shampoo.jpg",
+    tag: "💥 Deal of the Day".
+    category: "Hygiene"
   }
 ];
